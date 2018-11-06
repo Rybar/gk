668fd7026dde002523d872d98412578f34e1d302
rybar <ryan.malm@gmail.com>
2018-11-05 22:04:09 -0600

    some lib structure, container module

---
+++ gametin/index.js
@@ -0,0 +1,9 @@
+import KeyControls from "./controls/KeyControls.js";
+import MouseControls from "./controls/MouseControls.js";
+import Container from "./Container.js";
+
+export default {
+    KeyControls,
+    MouseControls,
+    Container
+};
--- src/canvasgame.js
+++ src/canvasgame.js
@@ -1,12 +1,12 @@
-import KeyControls from "/src/lib/KeyControls.js"
+import gt from "../gametin/index.js";
 
 const canvas = document.querySelector('#game canvas');
 const ctx = canvas.getContext('2d');
 console.log(ctx.canvas);
 const { width: w, height: h } = canvas;
 const center = w /2;
-const controls = new KeyControls();
-
+const controls = new gt.KeyControls();
+const mouse = new gt.MouseControls(canvas);
 let dt = 0;
 let last = 0;
 let x = w/2;
@@ -26,9 +26,10 @@ function loopy (ms) {
    ctx.strokeText(`Frame length: ${ (dt*1000).toFixed(2)} ms`, 70, 50);
    ctx.strokeText(`Total time: ${ t.toFixed(2)}`, 70, 90);
 
-    x += controls.x*2;
-    y += controls.y*2;
-    if (!controls.action) {
+    const x = mouse.pos.x;
+    const y = mouse.pos.y;
+
+    if (!mouse.pressed) {
         color += 10;
         if (color > 360) {
             color -= 360;
@@ -38,6 +39,8 @@ function loopy (ms) {
    ctx.fillStyle = `hsl(${color}, 50%, 50%)`;
    ctx.globalAlpha = 1;
    ctx.fillRect(x,y,50,50);
+
+   mouse.update();
 }
 
 requestAnimationFrame(loopy);
