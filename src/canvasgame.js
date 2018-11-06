871dccbcaf91764d16e131a41de907a71ff8ce03
rybar <ryan.malm@gmail.com>
2018-11-05 21:20:23 -0600

    keycontrols module

--- src/canvasgame.js
+++ src/canvasgame.js
@@ -1,42 +1,43 @@
+import KeyControls from "/src/lib/KeyControls.js"
+
 const canvas = document.querySelector('#game canvas');
 const ctx = canvas.getContext('2d');
 console.log(ctx.canvas);
 const { width: w, height: h } = canvas;
 const center = w /2;
-const img = new Image();
-img.src = 'res/img/rick.png';
-img.addEventListener('load',draw,false);
- 
+const controls = new KeyControls();
+
+let dt = 0;
+let last = 0;
+let x = w/2;
+let y = h/2;
+let color = 0;
+ctx.fillStyle = '#000';
+ctx.strokeStyle = '#fff';
 ctx.fillRect(0,0,w,h);
-ctx.fillStyle = '#555';
 
-let x, y, radius;
-for(let i = 0; i < 550; i++){
-    x = Math.random() * w;
-    y = Math.random() * h;
-    radius = Math.random() * 3;
-    ctx.beginPath();
-    ctx.arc(x,y, radius, 0, Math.PI * 2, false);
-    ctx.fill();
-}
-ctx.translate(w/2,h/2);
-for(let ring = 1; ring < 28; ring++){
-    ctx.fillStyle = `hsl(${ring * 25}, 90%, 50%)`;
-    for(let dots = 0; dots < ring * 6; dots++) {
-        
-        ctx.rotate((Math.PI * 2) / (ring * 6));
-        ctx.beginPath();
-        ctx.arc(0,ring * 15, 7, 0, Math.PI*2, true);
-        ctx.fill();
+function loopy (ms) {
+    requestAnimationFrame(loopy);
+
+    const t = ms / 1000;
+    dt = t - last;
+    last = t;
+
+   ctx.strokeText(`Frame length: ${ (dt*1000).toFixed(2)} ms`, 70, 50);
+   ctx.strokeText(`Total time: ${ t.toFixed(2)}`, 70, 90);
+
+    x += controls.x*2;
+    y += controls.y*2;
+    if (!controls.action) {
+        color += 10;
+        if (color > 360) {
+            color -= 360;
+        }
     }
+    
+   ctx.fillStyle = `hsl(${color}, 50%, 50%)`;
+   ctx.globalAlpha = 1;
+   ctx.fillRect(x,y,50,50);
 }
-ctx.restore();
 
-
-function draw() {
-    for(let i=0; i < 10; i++) {
-        const x = Math.random() * w - 50;
-        const y = Math.random() * h - 100;
-        ctx.drawImage(img,x,y);
-    }
-}
+requestAnimationFrame(loopy);
---
+++ src/lib/KeyControls.js
@@ -0,0 +1,51 @@
+class KeyControls {
+    constructor(){
+        this.keys = {};
+        //Bind event handlers
+        document.addEventListener('keydown',e =>{
+            if ([37,38,39,40].indexOf(e.which) >=0){
+                e.preventDefault();
+            }
+            this.keys[e.which] = true;
+
+        }, false);
+
+        document.addEventListener('keyup', e => {
+            this.keys[e.which] = false;
+        }, false);
+        
+
+    }
+
+    //handle key actions
+    get action () {
+        return this.keys[32];
+    }
+
+    get x () {
+        //left or A
+        if (this.keys[37] ||  this.keys[65]) {
+            return -1;
+        }
+        //right or D
+        if (this.keys[39] || this.keys[68]) {
+            return 1;
+        }
+        return 0;
+    }
+    
+    get y () {
+        //up or W
+        if(this.keys[38] || this.keys[87]){
+            return -1;
+        }
+        //down or S
+        if(this.keys[40] || this.keys[83]){
+            return 1;
+        }
+        return 0;
+    }
+}
+
+export default KeyControls;
+
