6c89677784c392d8f9c83d581d480ac8f57ee74f
rybar <ryan.malm@gmail.com>
2018-11-10 18:41:44 -0600

    MVP game framework complete, mock shooter jam game playable

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ document.querySelector(`#game`).appendChild(renderer.view);
 
 //game state
 let lastShot = 0;
+let lastSpawn = 0;
+let spawnSpeed = 1.0;
+let scoreAmount = 0;
+let gameOver = false;
 
 //game objects
 const scene = new Container();
@@ -16,10 +20,19 @@ const scene = new Container();
 const textures = {
     background: new Texture('../res/img/background2x.png'),
     spaceship: new Texture('../res/img/spaceship.png'),
-    bullet: new Texture('../res/img/bullet.png')
+    bullet: new Texture('../res/img/bullet.png'),
+    baddie: new Texture('../res/img/baddie.png')
 }
 
 const controls = new KeyControls();
+
+const score = new Text("score:", {
+    font: "20px sans-serif",
+    fill: "#DDD",
+    align: "center"
+});
+score.pos.x = w/2;
+score.pos.y = h-30;
 
 //make the player spaceship
 const ship = new Sprite(textures.spaceship);
@@ -36,7 +49,7 @@ ship.update = function (dt, t){
     if(pos.y < 0) pos.y = 0;
     if(pos.y > h-32) pos.y = h-32;
 
-    if(controls.action && t - lastShot > 0.15){
+    if(!gameOver && controls.action && t - lastShot > 0.15){
         lastShot = t;
         fireBullet(ship.pos.x + 24, ship.pos.y + 14);
     }
@@ -45,10 +58,25 @@ ship.update = function (dt, t){
 //bullets
 const bullets = new Container();
 
+//baddies
+const baddies = new Container();
+function spawnBaddie(x,y, speed){
+    const baddie = new Sprite(textures.baddie);
+    baddie.pos.x = x;
+    baddie.pos.y = y;
+    baddie.update = function(dt) {
+        this.pos.x += speed * dt;
+    }
+    baddies.add(baddie);
+}
+
+
 //add everything to scene container
 scene.add(new Sprite(textures.background));
 scene.add(ship);
 scene.add(bullets);
+scene.add(baddies);
+scene.add(score);
 
 
 //game loop
@@ -59,6 +87,34 @@ function loop (ms) {
     const t = ms / 1000;
     dt = t - last;
     last = t;
+    if(t-lastSpawn > spawnSpeed){
+        lastSpawn = t;
+        const speed = -50 - (Math.random() * Math.random() * 100);
+        const position = 24 + Math.random() * h - 48;
+        spawnBaddie(w, position, speed);
+
+        //accellerating spawn speed
+        spawnSpeed = spawnSpeed > 0.05 ?  0.6 : spawnSpeed * 0.97 + .001;
+    }
+    baddies.children.forEach(baddie => {
+        if(baddie.pos.x < -32) {
+            if(!gameOver){
+                doGameOver();
+            }
+            baddie.dead = true;
+        }
+        bullets.children.forEach(bullet => {
+            const dx = baddie.pos.x + 16 - (bullet.pos.x+8);
+            const dy = baddie.pos.y + 16 - (bullet.pos.y+8);
+            if(Math.hypot(dx,dy) < 24){
+                baddie.dead = true;
+                bullet.dead = true;
+                scoreAmount += Math.floor(t);
+            }
+        });
+    });
+    
+    score.text = `Score: ${scoreAmount}`; 
     scene.update(dt, t);
     renderer.render(scene);
 }
@@ -75,4 +131,17 @@ function fireBullet(x,y) {
         }
     }
     bullets.add(bullet);
+}
+
+function doGameOver() {
+    const gameOverMessage = new Text("Game Over", {
+        font: '30pt sans-serif',
+        fill: "#ff8888",
+        align: "center"
+    });
+    gameOverMessage.pos.x = w/2;
+    gameOverMessage.pos.y = 120;
+    scene.add(gameOverMessage);
+    scene.remove(ship);
+    gameOver = true;
 }
